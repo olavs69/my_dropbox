c49609d5c3a09662a2a837f3cac217c3271798c7
olav69 <124511804+olavs69@users.noreply.github.com>
2024-12-12 14:58:19 +0200

    add index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as FileList } from './FileList';
+export { default as FilePreview } from './FilePreview';
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as Footer } from './Footer';
+export { default as FileUpload } from './FileUpload';
+export { default as FolderTree } from './FolderTree';
